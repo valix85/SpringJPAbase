6e3d6119b8138e51b0e769ecfb06fce2f8c22645
Valerio <valerrio.radice@openstyle.it>
2017-07-03 17:08:16 +0200

    Set Entity and create Repository

--- src/main/java/springrestbase/Repository/TopicRepository.java
+++ src/main/java/springrestbase/Repository/TopicRepository.java
@@ -1,11 +1,16 @@
 package springrestbase.Repository;
 
 import org.springframework.data.repository.CrudRepository;
+import springrestbase.Entity.Topic;
 
 /**
  * Created by Valerio on 03/07/2017.
  */
-public interface TopicRepository extends CrudRepository {
+
+//Il repository è generico ed estende l'interfaccia di CrudRepository,
+//specificando la classe di cui si deve creare il Crud e il tipo del suo id
+
+public interface TopicRepository extends CrudRepository<Topic, Integer> {
 
     // il repository implementa l'interazione tra il service e l'entity
     // per esempio i metodi ottieni tutti e ottieni con id
@@ -13,6 +18,9 @@ public interface TopicRepository extends CrudRepository {
     // getTopic(Integer id)
     // updateTopic(Topic t)
     // deleteTopic(Integer id)
+    //
+    //
+    //... in realtà le classiche operazioni CRUD dell'interfacciao CrudRepository
 
 
 }
